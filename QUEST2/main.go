a1b28dbd8d26856aede62fc6c8f7f4c4a2772a61
Yunus Emre Yılmaz <yunusemreyilmaz347@gmail.com>
2023-01-02 12:32:06 +0300

    quest2

--- QUEST2/main.go
+++ QUEST2/main.go
@@ -2,36 +2,36 @@ package main
 
 import "fmt"
 
-//given input
+//Given input. Result is going to change proportionally to the input value
 var input int = 9
 
-//number of times the recursion will happen
-var x int = 5
+//Number of times the recursion will happen
+var runXTimes int = 5
 
-//counter that i will use inside the funciton.
-//since the recursive is going in reverse my index counter is also subtracts
-var c int = x + 1
+//Counter that i will use inside the funciton.
+//Since the recursive is going in reverse my index counter is also subtracts
+var counter int = runXTimes + 1
 
 //+1 is because function subtracts in the beginning of the function
 //so it wouldnt be equal to -1 when x is 0 and so on
 var result int
 
-func recursive(input int, x int, c int) int {
-	c--
+func recursive(input int, runXTimes int, counter int) int {
+	counter--
 	//b(0) valuation for recursive base function
-	if x == 0 {
+	if runXTimes == 0 {
 		result = 2
-		fmt.Printf("output b(%v):%v \n", x, result)
+		fmt.Printf("output b(%v):%v \n", runXTimes, result)
 		return 2
 	} else {
 		//b(n)=b(n-1)+(c*input/3)-1 is the recursive formula
-		result = recursive(input, x-1, c) + (c * input / 3) - 1
-		fmt.Printf("output b(%v) :%v \n", x, result)
+		result = recursive(input, runXTimes-1, counter) + (counter * input / 3) - 1
+		fmt.Printf("output b(%v) :%v \n", runXTimes, result)
 		return result
 	}
 }
 func main() {
 	fmt.Printf("input :%v \n", input)
-	recursive(input, x, c)
+	recursive(input, runXTimes, counter)
 
 }
